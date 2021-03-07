22ebc908dd7d0598a313637ec4a237afde3709d2
AGiesey <adamgiesey@gmail.com>
2021-03-07 08:23:31 -0500

    Added Firebase firestore

---
+++ src/components/AddRecipe.tsx
@@ -0,0 +1,74 @@
+import React, { useState } from 'react';
+import { database } from '../firebase';
+
+interface Recipe {
+  title: string;
+  ingredients: string;
+  directions: string;
+}
+
+export const AddRecipe = () =>
+{
+  const recipesRef = database.collection("recipes");
+  const [title, setTitle] = useState('');
+  const [ingredients, setIngredients] = useState('');
+  const [directions, setDirections] = useState('');
+
+  const onTitleChange = (e: any) => {
+    setTitle(e.target.value)
+  }
+
+  const onIngredientsChange = (e: any) => {
+    setIngredients(e.target.value);
+  }
+
+  const onDirectionsChange = (e: any) => {
+    setDirections(e.target.value);
+  }
+
+  const onSubmit = () => {
+    let recipe = {
+      title: title,
+      ingredients: ingredients,
+      directions: directions,
+    } as Recipe;
+
+    recipesRef.add(recipe);
+    setTitle('');
+    setIngredients('');
+    setDirections('');
+  }
+
+  return (
+    <>
+      <div className="row mb-5">
+        <div className="col">
+          <h1>Add Recipe</h1>
+        </div>
+      </div>
+      <div className="row mb-3">
+        <div className="col">
+          <label htmlFor="recipeTitle" className="form-label">Title</label>
+          <input type="text" id="recipeTitle" className="form-control" onChange={onTitleChange}/>
+        </div>
+      </div>
+      <div className="row mb-3">
+        <div className="col">
+          <label htmlFor="recipeIngredients" className="form-label">Ingredients</label>
+          <textarea className="form-control"id="recipeIngredients" onChange={onIngredientsChange}></textarea>
+        </div>
+      </div>
+      <div className="row mb-3">
+        <div className="col">
+          <label htmlFor="recipeDirections" className="form-label">Directions</label>
+          <textarea className="form-control" id="recipeDirections" onChange={onDirectionsChange}></textarea>
+        </div>
+      </div>
+      <div className="row">
+        <div className="col">
+          <button type="button" className="btn btn-primary" onClick={onSubmit}>Save</button>
+        </div>
+      </div>
+    </>
+  )
+}
---
+++ src/firebase.ts
@@ -0,0 +1,15 @@
+import firebase from 'firebase';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBCjy3sSh6uJcfx2bBGXxSAxvsnZv3F7bw",
+  authDomain: "adams-kitchen.firebaseapp.com",
+  projectId: "adams-kitchen",
+  //databaseURL: "https://adams-kitchen.firebaseio.com",
+  storageBucket: "adams-kitchen.appspot.com",
+  messagingSenderId: "194575382370",
+  appId: "1:194575382370:web:464673c44053f7b6179e3b"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+export const database = firebase.firestore();
