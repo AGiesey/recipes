b1a9e4b21c91d8cd9590793bbfcd66a55dbea238
AGiesey <adamgiesey@gmail.com>
2021-03-21 09:27:32 -0400

    Added basic tabs to add-recipe page

--- src/components/AddRecipe.tsx
+++ src/components/AddRecipe.tsx
@@ -1,12 +1,7 @@
 import React, { useState } from 'react';
 import { database, auth } from '../firebase';
-
-interface Recipe {
-  createdBy: string;
-  title: string;
-  ingredients: string;
-  directions: string;
-}
+import { Recipe } from '../models/Recipe';
+import './AddRecipe.scss';
 
 export const AddRecipe = () =>
 {
@@ -14,6 +9,7 @@ export const AddRecipe = () =>
   const [title, setTitle] = useState('');
   const [ingredients, setIngredients] = useState('');
   const [directions, setDirections] = useState('');
+  const [activeTab, setActiveTab] = useState('info');
 
   const onTitleChange = (e: any) => {
     setTitle(e.target.value)
@@ -27,13 +23,18 @@ export const AddRecipe = () =>
     setDirections(e.target.value);
   }
 
+  const onChangeTab = (e: React.MouseEvent<HTMLElement>) => {
+    const {id} = e.currentTarget;
+    setActiveTab(id)
+  }
+
+
   const onSubmit = () => {
     if (!title || !ingredients || !directions) return;
 
     console.log(auth.currentUser);
 
     let recipe = {
-      createdBy: auth.currentUser?.uid,
       title: title,
       ingredients: ingredients,
       directions: directions,
@@ -52,22 +53,41 @@ export const AddRecipe = () =>
           <h1>Add Recipe</h1>
         </div>
       </div>
-      <div className="row mb-3">
-        <div className="col">
-          <label htmlFor="recipeTitle" className="form-label">Title</label>
-          <input type="text" id="recipeTitle" className="form-control" onChange={onTitleChange}/>
+      <ul className="nav nav-tabs" id="myTab" role="tablist">
+        <li className="nav-item" role="presentation">
+          <button className={`nav-link ${activeTab === 'info' ? "active" : null}`} onClick={onChangeTab} id="info" type="button">Info</button>
+        </li>
+        <li className="nav-item" role="presentation">
+          <button className={`nav-link ${activeTab === 'ingredients' ? "active" : null}`} onClick={onChangeTab} id="ingredients" type="button">Ingredients</button>
+        </li>
+        <li className="nav-item" role="presentation">
+          <button className={`nav-link ${activeTab === 'directions' ? "active" : null}`} onClick={onChangeTab} id="directions" type="button">Directions</button>
+        </li>
+      </ul>
+      <div className="add-recipe-tabs tab-content">
+        <div className={`tab-pane fade ${activeTab === 'info' ? "show active" : null}`}>
+          <div className="row">
+            <div className="col">
+              <label htmlFor="recipeTitle" className="form-label">Title</label>
+              <input type="text" id="recipeTitle" className="form-control" onChange={onTitleChange}/>
+            </div>
+          </div>
         </div>
-      </div>
-      <div className="row mb-3">
-        <div className="col">
-          <label htmlFor="recipeIngredients" className="form-label">Ingredients</label>
-          <textarea className="form-control"id="recipeIngredients" onChange={onIngredientsChange}></textarea>
+        <div className={`tab-pane fade ${activeTab === 'ingredients' ? "show active" : null}`}>
+          <div className="row">
+            <div className="col">
+              <label htmlFor="recipeIngredients" className="form-label">Ingredients</label>
+              <textarea className="form-control"id="recipeIngredients" onChange={onIngredientsChange}></textarea>
+            </div>
+          </div>
         </div>
-      </div>
-      <div className="row mb-3">
-        <div className="col">
-          <label htmlFor="recipeDirections" className="form-label">Directions</label>
-          <textarea className="form-control" id="recipeDirections" onChange={onDirectionsChange}></textarea>
+        <div className={`tab-pane fade ${activeTab === 'directions' ? "show active" : null}`}>
+          <div className="row">
+            <div className="col">
+              <label htmlFor="recipeDirections" className="form-label">Directions</label>
+              <textarea className="form-control" id="recipeDirections" onChange={onDirectionsChange}></textarea>
+            </div>
+          </div>
         </div>
       </div>
       <div className="row">
@@ -78,3 +98,14 @@ export const AddRecipe = () =>
     </div>
   )
 }
+
+/*
+<label htmlFor="recipeTitle" className="form-label">Title</label>
+<input type="text" id="recipeTitle" className="form-control" onChange={onTitleChange}/>
+
+<label htmlFor="recipeIngredients" className="form-label">Ingredients</label>
+<textarea className="form-control"id="recipeIngredients" onChange={onIngredientsChange}></textarea>
+
+<label htmlFor="recipeDirections" className="form-label">Directions</label>
+<textarea className="form-control" id="recipeDirections" onChange={onDirectionsChange}></textarea>
+*/
